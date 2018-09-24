a218beac99238f4fa3cf6989b92ae9e3806605a0
Scott Clampet <scottclampet@Scotts-MacBook-Pro-2.local>
2018-09-24 07:51:24 -0600

    added ability for user to close keyboard when tapping outside of the create product form

--- product_manager/lib/pages/product_create.dart
+++ product_manager/lib/pages/product_create.dart
@@ -22,9 +22,8 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
   Widget _buildTitleTextField() {
     return TextFormField(
       decoration: InputDecoration(labelText: 'Product Title'),
-      autovalidate: true,
       validator: (String value) {
-        if(value.isEmpty || value.length < 5) {
+        if (value.isEmpty || value.length < 5) {
           return 'A title is required and should be 5+ characters long';
         }
       },
@@ -42,7 +41,7 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
       decoration: InputDecoration(labelText: 'Product Description'),
       maxLines: 4,
       validator: (String value) {
-        if(value.isEmpty || value.length < 10) {
+        if (value.isEmpty || value.length < 10) {
           return 'A description is required and should be 10+ characters long';
         }
       },
@@ -60,7 +59,8 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
       decoration: InputDecoration(labelText: 'Product Price'),
       keyboardType: TextInputType.number,
       validator: (String value) {
-        if(value.isEmpty || !RegExp(r'^(?:[1-9]\d*|0)?(?:\.\d+)?$').hasMatch(value)) {
+        if (value.isEmpty ||
+            !RegExp(r'^(?:[1-9]\d*|0)?(?:\.\d+)?$').hasMatch(value)) {
           return 'A price is required and should be a number';
         }
       },
@@ -75,7 +75,7 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
 
   void _submitForm() {
     //run validator code once the use hits save. 'validate()' returns a bool
-    if(!_formKey.currentState.validate()) {
+    if (!_formKey.currentState.validate()) {
       return;
     }
     //trigger all FormField onSaved methods
@@ -97,32 +97,40 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
     final double targetPadding = deviceWidth -
         targetWidth; //calculates the amount of space that remains as opposed to the width of the entire form
 
-    return Container(
-      margin: EdgeInsets.all(10.0),
-      child: Form(
-        key: _formKey,
-        child: ListView(
-          //used a listview so if the keyboard covers a textfield we can scroll and still enter info there.
-          padding: EdgeInsets.symmetric(horizontal: targetPadding / 2),
-          children: <Widget>[
-            _buildTitleTextField(),
-            _buildDescriptionTextField(),
-            _buildPriceTextField(),
-            SizedBox(height: 10.0),
-            RaisedButton(
-              child: Text('Save'),
-              textColor: Colors.white,
-              onPressed: _submitForm,
-            ),
-            // GestureDetector( //use this if you need more control over a button
-            //   onTap: _submitForm,
-            //   child: Container(
-            //     color: Colors.green,
-            //     padding: EdgeInsets.all(10.0),
-            //     child: Text('Button'),
-            //   ),
-            // ),
-          ],
+    return GestureDetector(
+      //register when the user taps anywhere but the keyboard
+      onTap: () {
+        //every TextField has an attached focus node that is used automatically when tapping it.
+        //to remove the focus node and close the keyboard - pass an empty focus node (remove the focus node).
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+      child: Container(
+        margin: EdgeInsets.all(10.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            //used a listview so if the keyboard covers a textfield we can scroll and still enter info there.
+            padding: EdgeInsets.symmetric(horizontal: targetPadding / 2),
+            children: <Widget>[
+              _buildTitleTextField(),
+              _buildDescriptionTextField(),
+              _buildPriceTextField(),
+              SizedBox(height: 10.0),
+              RaisedButton(
+                child: Text('Save'),
+                textColor: Colors.white,
+                onPressed: _submitForm,
+              ),
+              // GestureDetector( //use this if you need more control over a button
+              //   onTap: _submitForm,
+              //   child: Container(
+              //     color: Colors.green,
+              //     padding: EdgeInsets.all(10.0),
+              //     child: Text('Button'),
+              //   ),
+              // ),
+            ],
+          ),
         ),
       ),
     );
